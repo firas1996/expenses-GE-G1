96c4ba74f654b9b6000ddac561d9bca7ebb09448
firas hamila <firas.hm.1996@gmail.com>
2024-02-09 14:39:38 +0100

    date

--- src/Components/ExpenseItem.js
+++ src/Components/ExpenseItem.js
@@ -1,11 +1,11 @@
 import React from "react";
-
+import "./ExpenseItem.css";
 const ExpenseItem = () => {
   return (
-    <div>
+    <div className="expense-item">
       <div>date</div>
       <h2>title</h2>
-      <div>price</div>
+      <div className="expense-item__price">price</div>
     </div>
   );
 };
