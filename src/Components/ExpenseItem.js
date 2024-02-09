675b56f09669353b246c12e2f1b552ba9eeea5dc
firas hamila <firas.hm.1996@gmail.com>
2024-02-09 14:31:41 +0100

    az

---
+++ src/Components/ExpenseItem.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const ExpenseItem = () => {
+  return (
+    <div>
+      <div>date</div>
+      <h2>title</h2>
+      <div>price</div>
+    </div>
+  );
+};
+
+export default ExpenseItem;
