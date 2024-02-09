e48c71e7da79f532a20a6547aac987555a89a5d1
firas hamila <firas.hm.1996@gmail.com>
2024-02-09 16:04:31 +0100

    ss

---
+++ src/Components/DateItem.js
@@ -0,0 +1,13 @@
+import "./DateItem.css";
+
+const DateItem = () => {
+  return (
+    <div className="expense-date">
+      <div className="expense-date__month">month</div>
+      <div className="expense-date__year">year</div>
+      <div className="expense-date__day">day</div>
+    </div>
+  );
+};
+
+export default DateItem;
--- src/Components/ExpenseItem.js
+++ src/Components/ExpenseItem.js
@@ -1,9 +1,10 @@
 import React from "react";
 import "./ExpenseItem.css";
+import DateItem from "./DateItem";
 const ExpenseItem = ({ title, price, date }) => {
   return (
     <div className="expense-item">
-      <div>{date.toISOString()}</div>
+      <DateItem />
       <div className="expense-item__description">
         <h2>{title}</h2>
         <div className="expense-item__price">$ {price}</div>
