7a3d7f52e735f77acaae8102a9b6f260cd5803d1
firas hamila <firas.hm.1996@gmail.com>
2024-03-06 09:36:21 +0100

    dfgdf

--- src/Components/FormContainer/NewExpense.js
+++ src/Components/FormContainer/NewExpense.js
@@ -2,27 +2,49 @@ import { useState } from "react";
 import "./NewExpense.css";
 
 const NewExpense = () => {
-  const [title, setTitle] = useState("");
-  const [price, setPrice] = useState("");
-  const [date, setDate] = useState("");
+  // const [title, setTitle] = useState("");
+  // const [price, setPrice] = useState("");
+  // const [date, setDate] = useState("");
+  const [oneState, setOneState] = useState({
+    title: "",
+    price: "",
+    date: "",
+  });
   const titleChangeHandler = (event) => {
-    setTitle(event.target.value);
+    // setTitle(event.target.value);
+    // setOneState({ ...oneState, title: event.target.value });
+    setOneState((prevState) => {
+      return { ...prevState, title: event.target.value };
+    });
   };
   const priceChangeHandler = (event) => {
-    setPrice(event.target.value);
+    // setPrice(event.target.value);
+    setOneState({ ...oneState, price: event.target.value });
   };
   const dateChangeHandler = (event) => {
-    setDate(event.target.value);
+    // setDate(event.target.value);
+    setOneState({ ...oneState, date: event.target.value });
+  };
+  const inputChanger = (event) => {
+    setOneState((prevState) => {
+      return { ...prevState, title: event.target.value };
+    });
   };
 
   const submitForm = (event) => {
     event.preventDefault();
-    console.log(title);
-    console.log(price);
-    console.log(date);
-    setTitle("");
-    setPrice("");
-    setDate("");
+    console.log(oneState);
+    setOneState({
+      title: "",
+      price: "",
+      date: "",
+    });
+    // console.log(title);
+    // console.log(price);
+    // console.log(date);
+    // setTitle("");
+    // setPrice("");
+    // setDate("");
   };
   return (
     <div className="new-expense">
@@ -31,9 +53,9 @@ const NewExpense = () => {
           <div className="new-expense__control">
             <label>Title</label>
             <input
-              value={title}
               required
               placeholder="Title"
+              value={oneState.title}
               onChange={titleChangeHandler}
             />
           </div>
@@ -44,14 +66,14 @@ const NewExpense = () => {
               placeholder="Price"
               type="number"
               min="0"
-              value={price}
+              value={oneState.price}
               onChange={priceChangeHandler}
             />
           </div>
           <div className="new-expense__control">
             <label>Date</label>
             <input
-              value={date}
+              value={oneState.date}
               onChange={dateChangeHandler}
               required
               type="date"
